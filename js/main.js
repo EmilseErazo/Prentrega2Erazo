d76408b65c9b7f637ad5a1d86a4141158936536a
Emilse Erazo <emilsesilvana@gmail.com>
2024-04-22 21:37:08 -0300

    modificacion

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
-let productoCandy = [];
+let compraMensuales = [
+  {
+    categoria: "Brujita 1",
+    precio: 10000
+
+  },
+  {
+    categoria: "Brujita 2",
+    precio: 20000
+  },
+
+  {
+    categoria: "Brujita 3",
+    precio: 60000
+  }
+];
 
 let categoriaProductoAgregado;
 let precioProductoAgregado;
 do {
-    categoriaProductoAgregado = prompt ("Ingresa el producto elegido. Para terminar, escribe SALIR.");
-    precioProductoAgregado = parseInt(prompt("Ingresa el monto"));
+  categoriaProductoAgregado = prompt ("Ingresa el producto elegido. Para terminar, escribe SALIR.").toUpperCase().trim();
+
+ if (categoriaProductoAgregado !== "SALIR") {
+
+   precioProductoAgregado = parseInt(prompt("Ingresa el precio del producto"));
+   let productoAgregado = {
+       categoria: categoriaProductoAgregado,
+precio: precioProductoAgregado
+   };
+    compraMensuales.push(productoAgregado);
+    
+ }
+
 } while(categoriaProductoAgregado !== "SALIR");
+
+const calcularCompraTotal = () => {
+  let total = compraMensuales.reduce((acc, compra) => acc + compra.precio, 0);
+
+  console.log (total);
+}
+calcularCompraTotal();
+
